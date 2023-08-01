cbc34785dae3770c8db06a5bc77f512575a88487
Macguire Rintoul <git@macguire.me>
2023-08-01 08:33:57 -0700

    docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>Macguire's Docs</span>,
-  project: {
-    link: 'https://github.com/macguirerintoul/man.macguire.me',
-  }, 
-  docsRepositoryBase: 'https://github.com/macguirerintoul/man.macguire.me',
-  footer: {
-    text: "Macguire's docs",
-  },
-}
+	logo: <span>Macguire's Docs</span>,
+	project: {
+		link: "https://github.com/macguirerintoul/docs.macguire.me",
+	},
+	docsRepositoryBase: "https://github.com/macguirerintoul/docs.macguire.me",
+	footer: {
+		text: "Macguire's docs",
+	},
+	useNextSeoProps: () => {
+		const { asPath } = useRouter();
+		if (asPath !== "/") {
+			return {
+				titleTemplate: "%s • Macguire's Docs",
+			};
+		}
+	},
+	head: (
+		<>
+			<link
+				rel="icon"
+				href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📖</text></svg>"
+			/>
+		</>
+	),
+	sidebar: { defaultMenuCollapseLevel: 1 },
+};
 
-export default config
+export default config;
