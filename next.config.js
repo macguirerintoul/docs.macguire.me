09ff2525081af163e4727a34f778e414f8eaaf7f
Macguire Rintoul <git@macguire.me>
2024-02-13 11:43:48 -0800

    rm proxy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withNextra = require("nextra")({
 	theme: "nextra-theme-docs",
 	themeConfig: "./theme.config.tsx",
-});
-const { withPlausibleProxy } = require("next-plausible");
+}); 
 
-module.exports = withPlausibleProxy()(withNextra());
+module.exports = withNextra();
